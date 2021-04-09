9278a80a39e9cbb17976118be5a50b557e9f58b2
BuildTools <unconfigured@null.spigotmc.org>
2021-04-09 13:09:45 +0100

    Add project files.

---
+++ Post It Notes/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Post_It_Notes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.optionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addNoteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.deleteNoteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtNewTitle = new System.Windows.Forms.TextBox();
+            this.txtNewDesc = new System.Windows.Forms.TextBox();
+            this.txtNotes = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.optionsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(389, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // optionsToolStripMenuItem
+            // 
+            this.optionsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addNoteToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.deleteNoteToolStripMenuItem});
+            this.optionsToolStripMenuItem.Name = "optionsToolStripMenuItem";
+            this.optionsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionsToolStripMenuItem.Text = "Options";
+            // 
+            // addNoteToolStripMenuItem
+            // 
+            this.addNoteToolStripMenuItem.Name = "addNoteToolStripMenuItem";
+            this.addNoteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.addNoteToolStripMenuItem.Text = "Add Note";
+            this.addNoteToolStripMenuItem.Click += new System.EventHandler(this.addNoteToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // deleteNoteToolStripMenuItem
+            // 
+            this.deleteNoteToolStripMenuItem.Name = "deleteNoteToolStripMenuItem";
+            this.deleteNoteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteNoteToolStripMenuItem.Text = "Delete Note";
+            this.deleteNoteToolStripMenuItem.Click += new System.EventHandler(this.deleteNoteToolStripMenuItem_Click);
+            // 
+            // txtNewTitle
+            // 
+            this.txtNewTitle.Location = new System.Drawing.Point(13, 28);
+            this.txtNewTitle.Name = "txtNewTitle";
+            this.txtNewTitle.Size = new System.Drawing.Size(179, 20);
+            this.txtNewTitle.TabIndex = 1;
+            // 
+            // txtNewDesc
+            // 
+            this.txtNewDesc.Location = new System.Drawing.Point(12, 54);
+            this.txtNewDesc.Multiline = true;
+            this.txtNewDesc.Name = "txtNewDesc";
+            this.txtNewDesc.Size = new System.Drawing.Size(180, 225);
+            this.txtNewDesc.TabIndex = 2;
+            // 
+            // txtNotes
+            // 
+            this.txtNotes.Location = new System.Drawing.Point(198, 27);
+            this.txtNotes.Multiline = true;
+            this.txtNotes.Name = "txtNotes";
+            this.txtNotes.Size = new System.Drawing.Size(179, 252);
+            this.txtNotes.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 291);
+            this.Controls.Add(this.txtNotes);
+            this.Controls.Add(this.txtNewDesc);
+            this.Controls.Add(this.txtNewTitle);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NoteNET";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem optionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addNoteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem deleteNoteToolStripMenuItem;
+        private System.Windows.Forms.TextBox txtNewTitle;
+        private System.Windows.Forms.TextBox txtNewDesc;
+        private System.Windows.Forms.TextBox txtNotes;
+    }
+}
+
---
+++ Post It Notes/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Post_It_Notes
+{
+    public partial class Form1 : Form
+    {
+        List<Note> notes = new List<Note>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            openFile();
+        }
+
+        public void openFile()
+        {
+            if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\notes.txt"))
+            {
+                File.Create("notes.txt");
+                return;
+            }
+
+            StreamReader reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "\\notes.txt");
+
+            string[] file = reader.ReadToEnd().Split('|');
+
+            foreach(string s in file){
+                if(s != "")
+                {
+                    string[] split = s.Split('~');
+                    Note note = new Note();
+                    note.title = split[0].Split(':')[1];
+                    note.desc = split[1].Split(':')[1];
+                    notes.Add(note);
+                }
+            }
+
+            reader.Close();
+            reader.Dispose();
+
+            updateList();
+        }
+
+        public void updateList()
+        {
+            txtNotes.Text = "";
+
+            for(int i = 0; i < notes.Count; i++)
+            {
+                Note note = notes[i];
+                txtNotes.AppendText(("ID: " + i.ToString() + " ----------" + Environment.NewLine));
+                txtNotes.AppendText(("Name: " + note.title + Environment.NewLine));
+                txtNotes.AppendText(("Desc: " + note.desc + Environment.NewLine));
+                txtNotes.AppendText("" + Environment.NewLine);
+                txtNotes.AppendText("" + Environment.NewLine);
+                txtNotes.AppendText("" + Environment.NewLine);
+            }
+        }
+
+        public void saveFile()
+        {
+            StreamWriter writer = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "\\notes.txt");
+
+            foreach(Note note in notes){
+                writer.Write(("title:" + note.title + "~desc:" + note.desc + "|"));
+            }
+
+            writer.Close();
+            writer.Dispose();
+        }
+
+        private void addNoteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(txtNewTitle.Text != "" && txtNewDesc.Text != "")
+            {
+                Note note = new Note();
+                note.title = txtNewTitle.Text;
+                note.desc = txtNewDesc.Text;
+                notes.Add(note);
+            }
+
+            saveFile();
+            updateList();
+        }
+
+        private void deleteNoteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string input = Interaction.InputBox("Note ID", "Delete Note");
+            int id;
+            if(int.TryParse(input, out id)){
+                notes.RemoveAt(id);
+                saveFile();
+                updateList();
+            }
+        }
+    }
+
+    struct Note
+    {
+        public string title;
+        public string desc;
+    }
+}
